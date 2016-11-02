7ddcef69829a4f902e5b05a150f93428a040a38f
VictorAw <VictorAw4@gmail.com>
2016-11-01 17:40:45 -0700

    Fix rails routing syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  namespace api, defaults: {format: :json} do
-    resources :users, only [:index, :create, :update, :show, :destroy]
-    resource :session, only [:create, :destroy]
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:index, :create, :update, :show, :destroy]
+    resource :session, only: [:create, :destroy]
   end
 
   root :to => "static_pages#root"
