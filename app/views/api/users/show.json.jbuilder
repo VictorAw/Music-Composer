e9b8fa41f98ae3d37ae43870f4699b9746a877fc
VictorAw <VictorAw4@gmail.com>
2016-11-03 09:05:44 -0700

    Add persistent login between page refreshes

--- app/views/api/users/show.json.jbuilder
+++ app/views/api/users/show.json.jbuilder
@@ -1,9 +1,1 @@
-json.id @user.id
-json.username @user.username
-json.email @user.email
-json.description @user.description
-#json.set!(tracks) do
-#  @user.tracks.each do |track|
-#    json.extract!(track, :title, :id) 
-#  end
-#end
+json.partial! "api/users/user", user: @user
--- frontend/music_composer.jsx
+++ frontend/music_composer.jsx
@@ -4,7 +4,15 @@ import Root from "./components/root";
 import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
-  let store = configureStore();
+  let store;
+
+  if (window.currentUser) {
+    const preloadedState = {session: {currentUser: window.currentUser}};
+    store = configureStore(preloadedState); 
+  }
+  else {
+    store = configureStore();
+  }
 
   ReactDOM.render(<Root store={store} />, document.getElementById("root"));
 });
