28d6dc02c44eddb55ed2472186e7e00dfb8a431e
VictorAw <VictorAw4@gmail.com>
2016-11-02 19:11:58 -0700

    Add guest account to the database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create({username: "Guest", email: "guest@email.com", password: "tour_account", description: "Guest account"})
+
