34f0f3228294b6baa03145962ccc3da3e23e417b
VictorAw <VictorAw4@gmail.com>
2016-11-04 19:02:06 -0700

    Add back-end to tracks controller to add track start and end times to the database

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -3,7 +3,7 @@ class Api::TracksController < ApplicationController
     @track = Track.new(track_params)
     if @track.save
       if @track.update(channel_params)
-        new_times = {start_time: @track.start_time, end_time: @track.end_time}
+        new_times = {start_time: 0, end_time: @track.find_end_time}
         if @track.update(new_times)
           render :show
         else
@@ -22,7 +22,7 @@ class Api::TracksController < ApplicationController
     if @track
       if @track.update(track_params) && @track.update(channel_params)
         # Update start time and end time with new note data
-        new_times = {start_time: @track.start_time, end_time: @track.end_time}
+        new_times = {start_time: 0, end_time: @track.find_end_time}
         if @track.update(new_times)
           render :show
         else
@@ -39,7 +39,18 @@ class Api::TracksController < ApplicationController
 
   def show
     # Includes to prevent a ridiculous number of queries (n * m * o)
-    @track = Track.includes(channels: {chords: :notes}).find_by_id(params[:id])
+    # tracks = Track.find_by_sql(
+    # "SELECT DISTINCT * FROM ( \
+    #   SELECT * FROM tracks \
+    #   JOIN channels ON tracks.id = channels.track_id \
+    #   JOIN chords ON channels.id = chords.channel_id \
+    #   JOIN notes ON chords.id = notes.chord_id \
+    #    WHERE tracks.id = #{params[:id]} \
+    #    ORDER BY notes.start_time) AS track_data")
+    # ActiveRecord::Associations::Preloader.new.preload(@track, channels: {chords: :notes})
+    # @track = tracks.select { |track| track.id == params[:id].to_i }[0]
+    @track = Track.includes(channels: {chords: :notes}).order("notes.start_time ASC").find_by_id(params[:id])
+    #@track = Track.eager_load(channels: {chords: :notes}).order("notes.start_time ASC").find_by_id(params[:id])
     if @track
       render :show 
     else
