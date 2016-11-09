1ee756a0d3ef7ab26acf94b0c581ebf00b1c3bbf
VictorAw <VictorAw4@gmail.com>
2016-11-08 20:44:01 -0800

    Line up name column in schema creation migration

--- db/migrate/20161103162309_create_tracks.rb
+++ db/migrate/20161103162309_create_tracks.rb
@@ -2,7 +2,7 @@ class CreateTracks < ActiveRecord::Migration[5.0]
   def change
     create_table :tracks do |t|
       t.integer :composer_id, null: false
-      t.string :title, null: false
+      t.string  :title, null: false
       t.integer :bpm, null: false
       t.integer :start_time, null: false
       t.integer :end_time, null: false
